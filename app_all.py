858d671a74e58186c528799343e06e46e91dcf32
alexnesov <alexandrenesovic@gmail.com>
2022-07-02 14:44:59 +0200

    resolved login error due to blueprints implementation

--- app_all.py
+++ app_all.py
@@ -72,50 +72,6 @@ def welcome_user():
     return render_template('welcome_user.html')
 
 
-"""
-@page_all.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash('You logged out!')
-    return redirect(url_for('page_all.home'))
-"""
-
-"""
-@page_all.route('/login', methods=['GET', 'POST'])
-def login():
-
-    print('Logging in. . .')
-    form = LoginForm()
-
-    if form.validate_on_submit():
-        # Grab the user from our User Models table
-        user = User.query.filter_by(email=form.email.data).first()
-
-        # Check that the user was supplied and the password is right
-        # The verify_password method comes from the User object
-        # https://stackoverflow.com/questions/2209755/python-operation-vs-is-not
-
-        if user is None:
-            print('User is None')
-            return render_template('login.html', form = form, log_error = True)
-        elif user.check_password(form.password.data) and user is not None:
-            # Log in the user
-
-            login_user(user)
-            # If a user was trying to visit a page that requires a login
-            # flask saves that URL as 'next'.
-            next = request.args.get('next')
-
-            # So let's now check if that next exists, otherwise we'll go to
-            # the welcome page.
-            if next == None or not next[0] == '/':
-                next = url_for('page_all.welcome_user')
-
-            print("Next: ", next)
-            return redirect(next)
-    return render_template('login.html', form=form, log_error = False)
-"""
 
 @page_all.route("/account", methods=['GET', 'POST'])
 @login_required
